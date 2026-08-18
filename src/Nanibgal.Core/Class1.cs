f2bca8b90a14e8bf6ccd0fa58f209f810f964216
Jean Luca Colombo <jeanlucacolombo@outlook.com>
2026-08-18 17:48:52 -0300

    Initial Nanibgal solution

---
+++ src/Nanibgal.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Nanibgal.Core;
+
+public class Class1
+{
+
+}
---
+++ tests/Nanibgal.Core.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Nanibgal.Core.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
